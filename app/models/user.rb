39705fc3f4a0d0c6eb02069f5f7b5d0cd67daf66
CoderMukul <mukulsoni1211@gmail.com>
2024-04-17 07:20:08 +0530

    User model Added

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, :dob, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-]+(\[a-z]+)\.[a-z]+\z/i
+  validate :check_dob
+
+  def check_dob
+    unless dob.present? && dob < (Date.today - 18.year)
+      errors.add(:dob, 'You are not an adult person')
+    end
+  end
+end
